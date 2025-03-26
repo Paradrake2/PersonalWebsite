f048b3e8931de0fded6f4a09ca421a3bd3e32af6
Paradrake2 <57878896+Paradrake2@users.noreply.github.com>
2025-03-26 12:32:29 -0400

    added second project link

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
                     </a>
                 </div>
                 <div className="group w-96 mx-10 bg-black text-white px-6 py-1 rounded-lg transition-all duration-500 hover:bg-gray-600 cursor-pointer">
-                                    <a href="https://github.com/Paradrake2/PersonalWebsite" className="block w-full">
-                                    <p className="text-lg font-bold"> Project 1: This Website</p>
-                                    <p className="mt-2 text-sm max-h-0 opacity-0 group-hover:max-h-40 transition-all duration-300 group-hover:opacity-100">This is my personal portfolio website made with Next.js and Tailwind CSS! Click to view the GitHub Repository.</p>
-                                    </a>
-                                </div>
+                    <a href="https://github.com/Paradrake2/TextAdventure" className="block w-full">
+                    <p className="text-lg font-bold"> Project 2: Text Adventure game</p>
+                    <p className="mt-2 text-sm max-h-0 opacity-0 group-hover:max-h-40 transition-all duration-300 group-hover:opacity-100">This is going to be a playable text adventure fantasy game.</p>
+                    </a>
+                </div>
             </div>
         </body>
     );
