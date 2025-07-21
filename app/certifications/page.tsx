e2e2ad0f71f68a196f03e4a6ea3656eeea52ae94
Paradrake2 <57878896+Paradrake2@users.noreply.github.com>
2025-07-21 13:30:02 -0400

    added start of verilog/vdhl certification

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Navbar />
       <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
         <div>
-          <h1>Under construction</h1>
+          <h1>VDHL/Verilog Certification from AMD</h1>
         </div>
       </div>
     </body>
