89a84eeb547d99d299761cab3290c1d6aea54c70
Paradrake2 <57878896+Paradrake2@users.noreply.github.com>
2025-08-03 13:03:14 -0400

    project page update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -41,7 +41,16 @@ export default function Home() {
                     <a href="https://github.com/Paradrake2/CraftingDungeonCrawler" className="block w-full">
                         <div className="group">
                             <p className="text-lg font-bold"> Project 4: Unity game</p>
-                            <p className="mt-2 text-sm max-h-0 opacity-0 group-hover:max-h-40 group-hover:opacity-100 overflow-hidden transition-all duration-300">This is the extended version of the text adventure game, made in an actual game engine. This repo contains the assets for the game.</p>
+                            <p className="mt-2 text-sm max-h-0 opacity-0 group-hover:max-h-40 group-hover:opacity-100 overflow-hidden transition-all duration-300">!!CURRENTLY ON HOLD DUE TO LACK OF ART ASSETS!!   This is the extended version of the text adventure game, made in an actual game engine. This repo contains the assets for the game.</p>
+                        </div>
+                    </a>
+                </div>
+                <br></br>
+                <div className="group w-96 mx-10 bg-black text-white px-6 py-1 rounded-lg transition-all duration-500 hover:bg-gray-600 cursor-pointer">
+                    <a href="https://github.com/Paradrake2/VectorWarden" className="block w-full">
+                        <div className="group">
+                            <p className="text-lg font-bold"> Project 5: Vector Warden</p>
+                            <p className="mt-2 text-sm max-h-0 opacity-0 group-hover:max-h-40 group-hover:opacity-100 overflow-hidden transition-all duration-300">This is my original game called Vector Warden. It is a top down 2d shooter roguelite.</p>
                         </div>
                     </a>
                 </div>
@@ -51,6 +60,9 @@ export default function Home() {
                 </div>
                 <div className="px-6 py-1 rounded-lg w-auto mx-10 hover:text-gray-300">
                     <Link href="/projects/turingmachine" className=""> Turing machine simulator</Link>
+                </div>
+                <div className="px-6 py-1 rounded-lg w-auto mx-10 hover:text-gray-300">
+                    <Link href="/projects/vectorwarden" className=""> Vector Warden</Link>
                 </div>
             </div>
         </body>
