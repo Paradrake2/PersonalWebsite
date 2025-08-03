e174ad85d76a56e32c60a18e35b668b43157daa9
Paradrake2 <57878896+Paradrake2@users.noreply.github.com>
2025-08-03 13:09:29 -0400

    hopefully fixed video (isnt testing in production fun?)

--- app/projects/vectorwarden/page.tsx
+++ app/projects/vectorwarden/page.tsx
@@ -7,8 +7,6 @@ export default function Home() {
             <div className="flex flex-col h-screen bg-gradient-to-r from-black to-purple-500 p-6 text-white">
                 <p className="text-4xl text-white font-bold text-left px-10 py-5">Vector Warden</p>
                 <p className="mt-2 text-sm max-h-0 opacity-0 group-hover:max-h-40 group-hover:opacity-100 overflow-hidden transition-all duration-300">This is my original game called Vector Warden. It is a top down 2d shooter roguelite.</p>
-            </div>
-            <div>
                 <iframe
                     width="560"
                     height="315"
