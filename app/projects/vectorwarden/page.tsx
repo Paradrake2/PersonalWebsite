1cf37013aab374de438fe26a81cfb468eb7530ea
Paradrake2 <57878896+Paradrake2@users.noreply.github.com>
2025-08-03 13:07:43 -0400

    video fix

--- app/projects/vectorwarden/page.tsx
+++ app/projects/vectorwarden/page.tsx
@@ -8,15 +8,17 @@ export default function Home() {
                 <p className="text-4xl text-white font-bold text-left px-10 py-5">Vector Warden</p>
                 <p className="mt-2 text-sm max-h-0 opacity-0 group-hover:max-h-40 group-hover:opacity-100 overflow-hidden transition-all duration-300">This is my original game called Vector Warden. It is a top down 2d shooter roguelite.</p>
             </div>
-            <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/4J13q0AFJQM"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            ></iframe>
+            <div>
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/4J13q0AFJQM"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
+            </div>
         </body>
     )
 }
