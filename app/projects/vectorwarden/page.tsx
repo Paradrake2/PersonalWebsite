54d6f223fe8818e93a23a6863ba2a5de56420023
Paradrake2 <57878896+Paradrake2@users.noreply.github.com>
2025-08-04 22:01:46 -0400

    updated page to add second dev video

--- app/projects/vectorwarden/page.tsx
+++ app/projects/vectorwarden/page.tsx
@@ -16,6 +16,15 @@ export default function Home() {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                 ></iframe>
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/pMAvlBDtSzE"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
             </div>
         </body>
     )
