e5bda01fc193d5996d0bf6e501c1ddde1bbcd539
Paradrake2 <57878896+Paradrake2@users.noreply.github.com>
2025-08-17 22:06:47 -0400

    new progress video

--- app/projects/vectorwarden/page.tsx
+++ app/projects/vectorwarden/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <body>
             <Navbar />
-            <div className="flex flex-col h-screen bg-gradient-to-r from-black to-purple-500 p-6 text-white">
+            <div className="flex flex-row h-screen bg-gradient-to-r from-black to-purple-500 p-6 text-white">
                 <p className="text-4xl text-white font-bold text-left px-10 py-5">Vector Warden</p>
                 <p className="mt-2 text-sm max-h-0 opacity-0 group-hover:max-h-40 group-hover:opacity-100 overflow-hidden transition-all duration-300">This is my original game called Vector Warden. It is a top down 2d shooter roguelite.</p>
                 <iframe
@@ -29,6 +29,15 @@ export default function Home() {
                     width="560"
                     height="315"
                     src="https://www.youtube.com/embed/TstHIE_CDjY"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/QYlMMBdBHNo"
                     title="YouTube video player"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
