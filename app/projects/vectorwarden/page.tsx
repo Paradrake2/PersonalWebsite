450a3fc7d5f7e550e1177498aded403a12b20495
Paradrake2 <57878896+Paradrake2@users.noreply.github.com>
2025-11-01 15:19:55 -0400

    progress update

--- app/projects/vectorwarden/page.tsx
+++ app/projects/vectorwarden/page.tsx
@@ -55,6 +55,15 @@ export default function Home() {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                     ></iframe>
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/9Uey_cYiqIQ"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    ></iframe>
             </div>
         </body>
     )
